54009ac7a77c8390514cc1c4f465102c1af4f8b0
Fledra <baran@fledra.dev>
2025-03-14 03:30:37 +0300

    added THE content page

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
-    <div class="md:flex md:h-dvh">
+    <div class="overflow-x-hidden md:flex md:h-dvh">
       <Header class="w-screen md:w-2/5" />
       <main class="overflow-x-hidden overflow-y-auto md:w-3/5">
         <NuxtPage />
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,3 +1,30 @@
 <template>
-  <div>content page</div>
+  <div class="w-full overflow-x-hidden">
+    <template v-if="data">
+      <ContentRenderer :value="data" class="prose dark:prose-invert prose-a:no-underline w-full p-6 md:p-16 md:pr-8" />
+    </template>
+    <template v-else>
+      <div class="flex h-screen flex-col items-center justify-center">
+        <h1 class="mb-4 text-4xl font-semibold">{{ $t('notFound.heading') }}</h1>
+        <p class="mb-16 text-lg">{{ $t('notFound.text') }}</p>
+        <UButton :to="$localePath('/')" size="xl">{{ $t('notFound.button') }}</UButton>
+      </div>
+    </template>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import type { Collections } from '@nuxt/content';
+
+const route = useRoute();
+const { locale } = useI18n();
+const { data } = await useAsyncData(route.path, async () => {
+  const { slug } = route.params;
+
+  if (!Array.isArray(slug) || slug.length !== 2) return '';
+
+  const [collection, name] = slug as [keyof Collections, string];
+  const stem = `${collection}/${name}.${locale.value}`;
+  return queryCollection(collection).where('stem', '=', stem).first();
+});
+</script>
