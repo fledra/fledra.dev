7d09b337412221ffd06f15d0aade07a7414cbd87
Fledra <baran@fledra.dev>
2025-10-25 14:27:47 +0300

    fixed more tailwind canonical name warnings

--- app/components/Collection/CollectionRow.vue
+++ app/components/Collection/CollectionRow.vue
@@ -3,8 +3,8 @@
     <component
       :is="!props.disabled && props.to ? ULink : 'div'"
       :to="props.to"
-      :class="{ 'hover:text-(--ui-primary)': !props.disabled && props.to }"
-      class="flex w-fit gap-6 text-(--ui-text)"
+      :class="{ 'hover:text-primary': !props.disabled && props.to }"
+      class="text-default flex w-fit gap-6"
     >
       <UIcon v-if="props.icon" :name="props.icon" :customize="customizeIcon" class="h-7 w-7 transition-colors" />
 
@@ -12,7 +12,7 @@
         <span class="text-xl font-semibold transition-colors">
           {{ props.title }}
         </span>
-        <span v-if="props.description" class="text-xs text-(--ui-text-toned) dark:text-(--ui-text-muted)/50">
+        <span v-if="props.description" class="text-toned text-xs dark:text-(--ui-text-muted)/50">
           {{ props.description }}
         </span>
       </div>
